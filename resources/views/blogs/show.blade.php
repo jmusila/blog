af4332e8976e321df0913008a28167491b3441c8
jonathanmusila <jonathanmusila6@gmail.com>
2020-01-22 14:55:29 +0300

    combined all css file

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -5,13 +5,19 @@
     <div class="container-fluid">
     <article>
         <div class="jumbotron">
-            <h1>{{ $blog->title }}</h1>
-            <a class="btn btn-primary btn-xs pull-left" href="{{ route('blogs.edit', $blog->id) }}">Edit  </a>
-            <form action="{{ route('blogs.delete', $blog->id) }}" method="post">
-            @csrf
-                {{ method_field('delete') }}
-                <button type="submit" class="btn btn-danger btn-xs pull-left">Delete</button>
-            </form>
+            <div class="col-md-12">
+                <h1>{{ $blog->title }}</h1>
+            </div>
+            <div class="col-md-12">
+                <div class="btn-group">
+                    <a class="btn btn-primary btn-xs pull-left btn-margin-right" href="{{ route('blogs.edit', $blog->id) }}">Edit  </a>
+                    <form action="{{ route('blogs.delete', $blog->id) }}" method="post">
+                        @csrf
+                        {{ method_field('delete') }}
+                        <button type="submit" class="btn btn-danger btn-xs pull-left">Delete</button>
+                    </form>
+                </div>
+            </div>
         </div>
 
         <div class="colmd-12">
