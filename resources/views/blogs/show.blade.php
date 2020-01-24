2f8180aaab3d2943683febededae4195c19d2140
jonathanmusila <jonathanmusila6@gmail.com>
2020-01-24 13:02:11 +0300

    changed button styling

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -10,11 +10,11 @@
             </div>
             <div class="col-md-12">
                 <div class="btn-group">
-                    <a class="btn btn-primary btn-xs pull-left btn-margin-right" href="{{ route('blogs.edit', $blog->id) }}">Edit  </a>
+                    <a class="btn btn-primary btn-sm pull-left btn-margin-right" href="{{ route('blogs.edit', $blog->id) }}">Edit  </a>
                     <form action="{{ route('blogs.delete', $blog->id) }}" method="post">
                         @csrf
                         {{ method_field('delete') }}
-                        <button type="submit" class="btn btn-danger btn-xs pull-left">Delete</button>
+                        <button type="submit" class="btn btn-danger btn-sm pull-left">Delete</button>
                     </form>
                 </div>
             </div>
