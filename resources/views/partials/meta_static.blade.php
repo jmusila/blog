3c2a8ef6b72474cda1965d26d6430e9dc7305b45
jonathanmusila <jonathanmusila6@gmail.com>
2020-02-01 15:45:36 +0300

    added dynamic title

---
+++ resources/views/partials/meta_static.blade.php
@@ -0,0 +1,8 @@
+@section('meta_title')
+    Blogs about web development, PHP and Laravel
+@endsection
+
+@section('meta_description')
+    Best blogs about web development, PHP and Laravel. Advance your skills with our 
+    free resources.
+@endsection
